0b28bf96cde473d7a1e7d887eac2e05ac99c3a8d
Hanis Rasid <hanis.rasid@healthrfid.com>
2023-09-28 16:08:15 +1000

    setup pages and structure

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<!-- Build an online store and make sure it works locally-->
+<!-- Once it works locally, depoly it to aws -->
+
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+  <div>
+    <h1>This is the index page</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos mollitia rem at dolor temporibus cum animi similique repellat nobis minus esse qui, nemo alias consectetur necessitatibus rerum provident cupiditate adipisci?</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
