488b4f8c659da83340a3d6da281b325d7c9cac0f
Hanis Rasid <hanis.rasid@healthrfid.com>
2023-10-03 15:15:44 +1100

    fetch product data from backend server

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos mollitia rem at dolor temporibus cum animi similique repellat nobis minus esse qui, nemo alias consectetur necessitatibus rerum provident cupiditate adipisci?</p>
+    <p>Product details for {{ product[0].name }}</p>
+    <p>Price: {{ product[0].price }}</p>
+    <p>Quantity: {{ product[0].quantity }}</p>
   </div>
 </template>
 
 <script setup>
   const { id } = useRoute().params
+
+  definePageMeta({
+  layout: 'products'
+})
+  const {data: product} = await useFetch("http://localhost:8000/products/" + id, {key: id})
+
 </script>
 
 <style scoped>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <h1>Product main page</h1>
+  <div class="grid grid-cols-3 gap-5">
+    <div v-for="item in products">
+      <NuxtLink :to="`/products/${item.product_id}`">{{ item.name }}</NuxtLink>
+    </div>
   </div>
 </template>
 
 <script setup>
+  definePageMeta({
+    layout: 'products'
+  })
 
+  const {data: products} = await useFetch('http://localhost:8000/products', {
+    
+  })
 </script>
 
 <style lang="scss" scoped>
